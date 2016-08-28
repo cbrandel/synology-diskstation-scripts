504a642863427b9743a50cb9b1f58cf5b9037f1d
Christian Brandel <mail@cbrandel.de>
2016-08-28 12:14:39 +0200

    remove synologset

--- poll-dhcp-changes.sh
+++ poll-dhcp-changes.sh
@@ -3,16 +3,8 @@ ADMIN_DIR=$(dirname "$(readlink -f "$0")")
 . "$ADMIN_DIR/settings"
 ZoneRootDir=/var/packages/DNSServer/target
 ZonePath=$ZoneRootDir/named/etc/zone/master
+LOGFILE=/var/synolog/synosys.log
 ret=0
-# a bit clumsy, but this way the events will survive a firmware update
-EVENTSFILE=/usr/syno/synosdk/texts/enu/events
-if ! grep -q "90000000]" "$EVENTSFILE"; then
-  cat >> "$EVENTSFILE" <<EOF
-[90000000]
-90000001 = "Updated DNS from DHCP."
-90000002 = "Error Updating DNS from DHCP. Code: [@1]"
-EOF
-fi
 
 DHCP_TIME=$(date -d "$(stat /etc/dhcpd/dhcpd-leases.log | grep Modify | cut -d ' ' -f 2-)" "+%s")
 DNS_TIME=$(date -d "$(stat $ZonePath/$ForwardMasterFile | grep Modify | cut -d ' ' -f 2-)" "+%s")
@@ -21,11 +13,12 @@ then
   echo "dhcp leases changed - reloading DNS"
   $ADMIN_DIR/diskstation_dns_modify.sh
   ret=$?
+  ts=$(date "+%Y/%m/%d %H:%M:%S")
   if [ "$ret" -eq "0" ]
   then
-    synologset1 sys info 0x90000001
+    printf "info\t%s\tSYSTEM:\tUpdated DNS from DHCP." "$ts" >> $LOGFILE
   else
-    synologset1 sys err 0x90000002 $ret
+    printf "err\t%s\tSYSTEM:\tError Updating DNS from DHCP. Code: [%s]." "$ts" "$ret" >> $LOGFILE
   fi
 fi
 exit $ret
