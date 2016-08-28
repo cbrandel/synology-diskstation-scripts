b4d59d3cbfb732b6b644d85b8cf7ef662ed0ed70
Christian Brandel <mail@cbrandel.de>
2016-08-28 12:21:58 +0200

    fix log path and newline

--- poll-dhcp-changes.sh
+++ poll-dhcp-changes.sh
@@ -3,7 +3,7 @@ ADMIN_DIR=$(dirname "$(readlink -f "$0")")
 . "$ADMIN_DIR/settings"
 ZoneRootDir=/var/packages/DNSServer/target
 ZonePath=$ZoneRootDir/named/etc/zone/master
-LOGFILE=/var/synolog/synosys.log
+LOGFILE=/var/log/synolog/synosys.log
 ret=0
 
 DHCP_TIME=$(date -d "$(stat /etc/dhcpd/dhcpd-leases.log | grep Modify | cut -d ' ' -f 2-)" "+%s")
@@ -16,9 +16,9 @@ then
   ts=$(date "+%Y/%m/%d %H:%M:%S")
   if [ "$ret" -eq "0" ]
   then
-    printf "info\t%s\tSYSTEM:\tUpdated DNS from DHCP." "$ts" >> $LOGFILE
+    printf "info\t%s\tSYSTEM:\tUpdated DNS from DHCP.\n" "$ts" >> $LOGFILE
   else
-    printf "err\t%s\tSYSTEM:\tError Updating DNS from DHCP. Code: [%s]." "$ts" "$ret" >> $LOGFILE
+    printf "err\t%s\tSYSTEM:\tError Updating DNS from DHCP. Code: [%s].\n" "$ts" "$ret" >> $LOGFILE
   fi
 fi
 exit $ret
